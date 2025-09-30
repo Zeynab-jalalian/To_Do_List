a829856f0587e9428e13df55880efa426d58b8b2
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-09-30 10:26:44 +0330

    js part1

---
+++ script.js
@@ -0,0 +1,34 @@
+const taskInput = document.querySelector(".input");
+const addBtn = document.querySelector(".add-btn");
+const error = document.querySelector(".error");
+const tasks = document.querySelector(".tasks");
+
+const newTask = () => {
+  const taskVal = taskInput.value.trim();
+  error.style.display = "none";
+  if (!taskVal) {
+    error.style.display = "block";
+    taskInput.focus(); 
+    return;
+  }
+  const task = `
+  <div class="task">
+          <input type="checkbox" class="checkbox" />
+          <p class="taskName">${taskVal}</p>
+          <div class="edit">
+            <i class="bi bi-pencil-square"></i>
+          </div>
+          <div class="delete">
+            <i class="bi bi-trash"></i>
+          </div>
+          <div class="star">
+            <i class="bi bi-star"></i>
+          </div>
+        </div>
+  `;
+  tasks.insertAdjacentHTML("beforeend", task);
+  taskInput.value = "";
+  taskInput.focus();
+};
+
+addBtn.addEventListener("click", newTask);
